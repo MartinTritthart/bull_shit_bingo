a8d073ca48ff2ec62f3703f156bd47d6c79706ae
MartinTritthart <martin.tritthart@student.tugraz.at>
2023-12-22 21:59:20 +0100

    adding subprocess to run latex from main.py

--- bull_shit_bingo/io.py
+++ bull_shit_bingo/io.py
@@ -1,4 +1,8 @@
+import pandas as pd
 import json
+import subprocess
+
+# pandas table formate
 
 
 def read_json(prompt: str, file_path: str = "input/prompts.json") -> list:
@@ -8,8 +12,16 @@ def read_json(prompt: str, file_path: str = "input/prompts.json") -> list:
 
 
 def write_to_latex(df, saving_path: str = "output/table.tex"):
-    latex_table = df.to_latex()
+    latex_table = df.to_latex(index=False, escape=False, header=False)
     with open(saving_path, "w") as file:
         file.write(latex_table)
 
     print("Latex table saved")
+
+
+def running_latex(file_path: str):
+    try:
+        subprocess.run(["pdflatex", file_path], check=True)
+        print("PDF generated")
+    except subprocess.CalledProcessError as e:
+        print(f"Error during compilation: {e}")
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from bull_shit_bingo import bingo_table
 
 
 def main():
-    a = io.read_json("christmess_prompts_2023")
-    print(a)
-    table = bingo_table.generate_bingo_table(a)
-    print(table)
+    prompts = io.read_json("christmess_prompts_2023")
+    table = bingo_table.generate_bingo_table(prompts)
     io.write_to_latex(table)
+    io.running_latex("bull_shit_bingo.tex")
 
 
 if __name__ == "__main__":
